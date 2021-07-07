ac28423973629b129cf1a5688c11b1e3be90f149
Kristian Vassilev <kvasilev@devrix.com>
2021-07-07 12:00:02 +0300

    Added post type check and formatted the code

--- my-onboarding-plugin.php
+++ my-onboarding-plugin.php
@@ -12,18 +12,26 @@ if(!defined('ABSPATH')) {
     exit;
 }
 
-$is_plugin_enabled = get_option('onboarding_enabled');
 
 // Adds a menu element to the admin_menu for the plugin
+
 add_action('admin_menu', 'add_plugin_menu');
+
 function add_plugin_menu(){
+
     //title, name in menu, capability, title of page inside, name of function, dash-icon
     add_menu_page('My Onboarding', 'My Onboarding', 'administrator', 'ob-filter', 'ob_add_filter', 'dashicons-palmtree');
-}
+};
 
-// Adds checkbox
+
+// Adds checkbox and checks whether the plugin is enabled
+
+$is_plugin_enabled = get_option('onboarding_enabled');
+
 function ob_add_filter(){
+
     //get the current option value from db
+
     $is_plugin_enabled = get_option('onboarding_enabled');
     $form_check = "";
 
@@ -31,7 +39,7 @@ function ob_add_filter(){
     $form_check = "checked";
     }   else{
     $form_check = "";
-    }
+    };
 
     $ob_check = '
         <h2>Enable/Disable filter</h2>
@@ -43,46 +51,75 @@ function ob_add_filter(){
 };
 
 
+//Makes sure the plugin runs only if enabled
+
 if($is_plugin_enabled == 'true'){
-    // Adds a string to the wp_head element
-    add_action("wp_head", "onboarding_plugin");
-    function onboarding_plugin() {
-        echo 'Onboarding Filter by: kvasilev';
-    };
     
-    // Adds a hidden div after the first </p>
-    add_filter('the_content', 'add_hidden_div');
-    function add_hidden_div($content) { 
-    $content = str_replace('</p>', '</p><div style="display:none;">hidden div</div>', $content);
-    echo $content;
-    };
 
-    // Adds a paragrah and a hidden div
-    add_filter('the_content', 'add_new_paragraph');
-    function add_new_paragraph($content) {
-    $content= '<p>Generic paragraph</p>';
-    $content = str_replace('<p>Generic paragraph</p>', '<p>Generic paragraph <div style="display:none;">hidden div</div></p>', $content);
-    echo $content;
-    };
+    // Adds a string to the wp_head element
 
-    // Emailing functionality upon profile update
-    add_action('profile_update', 'update_profile_email'); 
-    function update_profile_email() {
-    $myUser = get_currentuserinfo();
-    //wp_mail(email-to, email-subject, email-message)
-    wp_mail('kvasilev@devrix.com', 'New profile update', 'Profile with user name '.$myUser->user_login.' has been updated.');
-    };
+    add_action("wp_head", "onboarding_plugin");
 
-    // Adds a nav element (profile page) to the current main menu if user is logged
-    add_filter('wp_nav_menu_items','add_profile_page');
-    function add_profile_page($items){
-        if(!is_user_logged_in()){
-            return $items;
-        }else if(is_user_logged_in()){
-            $profilePageLink = admin_url('profile.php');
-            return $items .= '<li><a href="'.$profilePageLink.'" class = "menu-item" >Profile Settings</a></li>';
+    function onboarding_plugin() {
+        if(get_post_type() == 'student'){
+            echo 'Onboarding Filter by: kvasilev';
         };
     };
+    
+    
+    // Adds a hidden div after the first </p>
+
+    add_filter('the_content', 'add_hidden_div');
+    
+    function add_hidden_div($content) { 
+        if(get_post_type() == 'student'){
+            $content = str_replace('</p>', '</p><div style="display:none;">hidden div</div>', $content);
+            echo $content;
+        }else {
+            echo $content;
+        };
+    };
+
+
+    // Adds a paragrah and a hidden div
+
+    add_filter('the_content', 'add_new_paragraph');
+
+    function add_new_paragraph($content) {
+        if(get_post_type() == 'student'){
+            $content= '<p>Generic paragraph</p>';
+            $content = str_replace('<p>Generic paragraph</p>', '<p>Generic paragraph <div style="display:none;">hidden div</div></p>', $content);
+            echo $content;
+        }else{
+            echo $content;
+        }
+    };
+
+
+    // Emailing functionality upon profile update
+
+    add_action('profile_update', 'update_profile_email');
+
+    function update_profile_email() {
+        $myUser = get_currentuserinfo();
+        //wp_mail(email-to, email-subject, email-message)
+        wp_mail('kvasilev@devrix.com', 'New profile update', 'Profile with user name '.$myUser->user_login.' has been updated.');
+    };
+
+
+    // Adds a nav element (profile page) to the current main menu if user is logged
+
+    add_filter('wp_nav_menu_items','add_profile_page');
+
+    function add_profile_page($items){
+        if(is_user_logged_in() && get_post_type() == 'student'){
+            $profilePageLink = admin_url('profile.php');
+            return $items .= '<li><a href="'.$profilePageLink.'" class = "menu-item" >Profile Settings</a></li>';
+        }else{
+            return $items;
+        }
+    };
+
 };
 
 // Imports and starts the addOption.js
@@ -93,6 +130,9 @@ function add_onboarding_script() {
     
 add_action( 'admin_enqueue_scripts', 'add_onboarding_script' );  
 add_action( 'wp_ajax_add_options', 'add_options' );
+
+
+//Adds options to save the plugin enable/disable state into the db
 
 function add_options() {
     $currentValue =  $_POST['ob_filter'];
