9892bbcbd992e633206ef029be48ef05f4f7ccbf
Kristian Vassilev <kvasilev@devrix.com>
2021-07-14 11:03:17 +0300

    functions formatting

--- my-onboarding-plugin.php
+++ my-onboarding-plugin.php
@@ -24,34 +24,34 @@ add_action( 'admin_menu', 'add_plugin_menu' );
  * @return void
  */
 function add_plugin_menu() {
-	add_menu_page( 'My Onboarding', 'My Onboarding', 'administrator', 'ob-filter', 'ob_add_filter', 'dashicons-palmtree' );
+	add_menu_page( 'My Onboarding', 'My Onboarding', 'administrator', 'ob-filter', 'ob_enable_filter', 'dashicons-palmtree' );
 };
 
 
 
 /**  Adds checkbox and checks whether the plugin is enabled */
-$is_plugin_enabled = get_option( 'onboarding_enabled' );
+$ob_is_plugin_enabled = get_option( 'onboarding_enabled' );
 
 /**
  * Adds an enable/disable filter to the plugin and checks its value
  *
  * @return void
  */
-function ob_add_filter() {
+function ob_enable_filter() {
 	// get the current option value from db.
-	$is_plugin_enabled = get_option( 'onboarding_enabled' );
-	$form_check        = '';
+	$ob_is_plugin_enabled = get_option( 'onboarding_enabled' );
+	$ob_form_check        = '';
 
-	if ( 'true' === $is_plugin_enabled ) {
-		$form_check = 'checked';
+	if ( 'true' === $ob_is_plugin_enabled ) {
+		$ob_form_check = 'checked';
 	} else {
-		$form_check = '';
+		$ob_form_check = '';
 	};
 
 	$ob_check = '
         <h2>Enable/Disable filter</h2>
         <div>
-            <input type="checkbox" id="ob-filter" name="ob-filter" ' . $form_check;'>
+            <input type="checkbox" id="ob-filter" name="ob-filter" ' . $ob_form_check;'>
             <label for="ob-filter">Filters Enabled</label>
         </div>';
 	echo ( $ob_check );
@@ -59,7 +59,7 @@ function ob_add_filter() {
 
 
 // Makes sure the plugin runs only if enabled.
-if ( 'true' === $is_plugin_enabled ) {
+if ( 'true' === $ob_is_plugin_enabled ) {
 
 
 	// Adds a string to the wp_head element.
@@ -78,7 +78,7 @@ if ( 'true' === $is_plugin_enabled ) {
 
 
 	// Adds a hidden div after the first </p>.
-	add_filter( 'the_content', 'add_hidden_div', 50 );
+	add_filter( 'the_content', 'ob_add_hidden_div', 50 );
 
 	/**
 	 * Adds a hidden div after the closing tag </p>
@@ -86,7 +86,7 @@ if ( 'true' === $is_plugin_enabled ) {
 	 * @param string $content of the wp post.
 	 * @return void
 	 */
-	function add_hidden_div( $content ){
+	function ob_add_hidden_div( $content ){
 		if ( get_post_type() === 'student' ) {
 			$content = str_replace( '</p>', '</p><div style="display:none;">hidden div</div>', $content );
 			echo ( $content );
@@ -97,7 +97,7 @@ if ( 'true' === $is_plugin_enabled ) {
 
 
 	// Adds a paragrah and a hidden div.
-	add_filter( 'the_content', 'add_new_paragraph', 60 );
+	add_filter( 'the_content', 'ob_add_new_paragraph', 60 );
 
 	/**
 	 * Adds new paragraph with hidden div after the content
@@ -105,7 +105,7 @@ if ( 'true' === $is_plugin_enabled ) {
 	 * @param string $content of the wp post.
 	 * @return void
 	 */
-	function add_new_paragraph( $content ) {
+	function ob_add_new_paragraph( $content ) {
 		if ( get_post_type() === 'student' ) {
 			$content = '<p>Generic paragraph</p>';
 			$content = str_replace( '<p>Generic paragraph</p>', '<p>Generic paragraph <div style="display:none;">hidden div</div></p>', $content );
@@ -116,14 +116,14 @@ if ( 'true' === $is_plugin_enabled ) {
 	};
 
 	// Emailing functionality upon profile update.
-	add_action( 'profile_update', 'update_profile_email' );
+	add_action( 'profile_update', 'ob_update_profile_email' );
 
 	/**
 	 * Sends and email upon profile update
 	 *
 	 * @return void
 	 */
-	function update_profile_email() {
+	function ob_update_profile_email() {
 		$my_user = wp_get_current_user();
 
 		// wp_mail(email-to, email-subject, email-message).
@@ -131,7 +131,7 @@ if ( 'true' === $is_plugin_enabled ) {
 	};
 
 	// Adds a new filter to the menu for showing (profile page).
-	add_filter( 'wp_nav_menu_items', 'add_profile_page' );
+	add_filter( 'wp_nav_menu_items', 'ob_add_profile_settings_page' );
 
 	/**
 	 * Adds profile page if user is logged in.
@@ -139,7 +139,7 @@ if ( 'true' === $is_plugin_enabled ) {
 	 * @param string $items are the menu.
 	 * @return void
 	 */
-	function add_profile_page( $items ) {
+	function ob_add_profile_settings_page( $items ) {
 		if ( is_user_logged_in() && get_post_type() === 'student' ) {
 			$profile_page_link = admin_url( 'profile.php' );
 			return $items     .= '<li><a href="' . $profile_page_link . '" class = "menu-item" >Profile Settings</a></li>';
@@ -150,17 +150,17 @@ if ( 'true' === $is_plugin_enabled ) {
 };
 
 /**  Imports and starts the addOption.js */
-function add_onboarding_script() {
+function ob_add_onboarding_script() {
 	wp_enqueue_script( 'addOptions', plugins_url( 'addOptions.js', __FILE__ ), array( 'jquery' ), false, true );
 	wp_localize_script( 'addOptions', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 };
 
-add_action( 'admin_enqueue_scripts', 'add_onboarding_script' );
-add_action( 'wp_ajax_add_options', 'add_options' );
+add_action( 'admin_enqueue_scripts', 'ob_add_onboarding_script' );
+add_action( 'wp_ajax_add_options', 'ob_add_options' );
 
 
 /** Adds options to save the plugin enable/disable state into the db */
-function add_options() {
+function ob_add_options() {
 	$current_value = wp_unslash( sanitize_text_field( $_POST['ob_filter'] ) );
 	$saved_option  = '';
 
